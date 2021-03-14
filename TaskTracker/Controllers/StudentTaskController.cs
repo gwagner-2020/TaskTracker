8f431cbdb6f7db0b065fd4ae22760c7622d894ff
Gretchen Wagner <gretchenwag@gmail.com>
2021-03-14 18:16:05 -0500

    task add and delete functionality with minimal task properties

---
+++ TaskTracker/Controllers/StudentTaskController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TaskTracker.Data;
+using TaskTracker.Models;
+using TaskTracker.ViewModels;
+
+namespace TaskTracker.Controllers
+{
+    public class StudentTaskController : Controller
+    {
+        //static private List<StudentTask> studentTasks = new List<StudentTask>();
+        
+        [HttpGet]
+        public IActionResult Index()
+        {
+            //studentTasks.Add("Clean Desk");
+            //studentTasks.Add("Monitor Gallery");
+            //ViewBag.studentTasks = StudentTaskData.GetAll();
+            List<StudentTask> studentTasks = new List<StudentTask>(StudentTaskData.GetAll());
+
+            return View(studentTasks);
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            AddStudentTaskViewModel addStudentTaskViewModel = new AddStudentTaskViewModel();
+            return View(addStudentTaskViewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Add(AddStudentTaskViewModel addStudentTaskViewModel)
+        {
+            StudentTask newStudentTask = new StudentTask
+            {
+                Name = addStudentTaskViewModel.Name,
+                Description = addStudentTaskViewModel.Description
+            };
+            StudentTaskData.Add(newStudentTask);
+            return Redirect("/StudentTask");
+        }
+
+        [HttpGet]
+        public IActionResult Delete()
+        {
+            ViewBag.studentTasks = StudentTaskData.GetAll();
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int[] studentTaskIds)
+        {
+            foreach(int studentTaskId in studentTaskIds)
+            {
+                StudentTaskData.Remove(studentTaskId);
+            }
+
+            return Redirect("/StudentTask");
+        }
+    }
+}
---
+++ TaskTracker/Models/StudentTask.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TaskTracker.Models
+{
+    public class StudentTask
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public int Id { get; set; }
+        private static int nextId = 1;
+
+        public StudentTask()
+        {
+            Id = nextId;
+            nextId++;
+        }
+        
+        public StudentTask(string name, string description) : this()
+        {
+            this.Name = name;
+            this.Description = description;
+        }
+
+        public override string ToString()
+        {
+            return this.Name;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is StudentTask @studentTask &&
+                Id == studentTask.Id;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id);
+        }
+    }
+}
